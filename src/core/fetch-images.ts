1e1973aa40dae857998edc73e47deca9bad712eb
Raku Zeta <zetaraku@gmail.com>
2022-04-30 12:15:02 +0800

    ✨ core/fetch-images: add optional header param

--- src/core/fetch-images.ts
+++ src/core/fetch-images.ts
@@ -8,12 +8,16 @@ export default async function run(
   songs: Record<string, any>[],
   imageDir: string,
   logger: log4js.Logger,
+  headers: Record<string, string> = {},
 ) {
   logger.info('Downloading cover image for songs ...');
   for (const [index, song] of songs.entries()) {
     if (song.imageName && !fs.existsSync(`${imageDir}/${song.imageName}`)) {
       logger.info(`(${1 + index} / ${songs.length}) ${song.title}`);
-      await download(song.imageUrl, imageDir, { filename: song.imageName });
+      await download(song.imageUrl, imageDir, {
+        filename: song.imageName,
+        headers,
+      });
 
       await sleep(100);
     }
