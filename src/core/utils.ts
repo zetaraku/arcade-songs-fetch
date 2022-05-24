dee5cd2aac48176253e4a6cfdeb18a9c9cd34631
Raku Zeta <zetaraku@gmail.com>
2022-05-24 13:20:12 +0800

    🔥 core/utils: remove useless logger

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,9 +1,6 @@
 import crypto from 'crypto';
 import log4js from 'log4js';
 
-const logger = log4js.getLogger('core/utils');
-logger.level = log4js.levels.INFO;
-
 export function hashed(text: string): string {
   return crypto.createHash('sha256').update(text).digest('hex');
 }
