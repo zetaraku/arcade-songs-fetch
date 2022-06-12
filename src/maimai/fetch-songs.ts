b8afee930681d1674d9c58e7a4c57ae944067276
Raku Zeta <zetaraku@gmail.com>
2022-06-12 11:41:45 +0800

    🔥 maimai/fetch-songs: remove extra songs loading code

--- src/maimai/fetch-songs.ts
+++ src/maimai/fetch-songs.ts
@@ -1,7 +1,4 @@
-import fs from 'fs';
 import axios from 'axios';
-// eslint-disable-next-line import/no-unresolved
-import { parse as parseCsv } from 'csv/sync';
 import log4js from 'log4js';
 import { Song, Sheet, JpSheet } from './models';
 import { hashed, ensureNoDuplicateEntry } from '../core/utils';
@@ -11,7 +8,6 @@ logger.level = log4js.levels.INFO;
 
 const DATA_URL = 'https://maimai.sega.jp/data/maimai_songs.json';
 const IMAGE_BASE_URL = 'https://maimaidx.jp/maimai-mobile/img/Music/';
-const DATA_DIR_PATH = 'data/maimai';
 
 const versionMap = new Map([
   [0, null],
@@ -112,25 +108,11 @@ export default async function run() {
   const rawSongs: Record<string, any>[] = response.data;
   logger.info(`OK, ${rawSongs.length} songs fetched.`);
 
-  const rawExtraSongs: Record<string, any>[] = (() => {
-    if (fs.existsSync(`${DATA_DIR_PATH}/extra-songs.tsv`)) {
-      logger.info(`Loading extra songs from: ${DATA_DIR_PATH}/extra-songs.tsv ...`);
-      const rawTsv = fs.readFileSync(`${DATA_DIR_PATH}/extra-songs.tsv`, 'utf8');
-      const result = parseCsv(rawTsv, { delimiter: '\t', columns: true });
-      logger.info(`OK, ${result.length} extra songs loaded.`);
-      return result;
-    }
-    return [];
-  })();
-
-  const allRawSongs = [...rawExtraSongs, ...rawSongs];
-
   logger.info('Ensuring every song has an unique songId ...');
-  ensureNoDuplicateEntry(allRawSongs.map((rawSong) => getSongId(rawSong)));
+  ensureNoDuplicateEntry(rawSongs.map((rawSong) => getSongId(rawSong)));
 
-  const songs = allRawSongs.map((rawSong) => extractSong(rawSong));
-  const sheets = allRawSongs.flatMap((rawSong) => extractSheets(rawSong));
-  const jpSheets = rawSongs.flatMap((rawSong) => extractSheets(rawSong));
+  const songs = rawSongs.map((rawSong) => extractSong(rawSong));
+  const sheets = rawSongs.flatMap((rawSong) => extractSheets(rawSong));
 
   logger.info('Preparing Songs table ...');
   await Song.sync();
@@ -148,7 +130,7 @@ export default async function run() {
   await Promise.all(sheets.map((sheet) => Sheet.upsert(sheet)));
 
   logger.info('Inserting jpSheets ...');
-  await JpSheet.bulkCreate(jpSheets);
+  await JpSheet.bulkCreate(sheets);
 
   logger.info('Done!');
 }
