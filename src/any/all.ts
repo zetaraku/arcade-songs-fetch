767d72c873136251dd2b9736c1c24cb5057935a4
Raku Zeta <zetaraku@gmail.com>
2022-06-15 22:45:08 +0800

    🔥 remove */all scripts

--- src/any/all.ts
+++
@@ -1,9 +0,0 @@
-import genJson from './gen-json';
-import uploadData from './upload-data';
-
-export default async function run() {
-  await genJson();
-  await uploadData();
-}
-
-if (require.main === module) run();
