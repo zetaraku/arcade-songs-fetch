f97fa3b8738fde00ec08b917a4be8872083ed325
Raku Zeta <zetaraku@gmail.com>
2022-06-23 12:48:22 +0800

    ✨ add core/gen-json

---
+++ src/core/gen-json.ts
@@ -0,0 +1,85 @@
+import { getSheetSorter } from './utils';
+
+export default async function run({
+  songRecords,
+  sheetRecords,
+  categories,
+  versions,
+  types,
+  difficulties,
+  regions,
+  updateTime = new Date(),
+  getLevelValueOf,
+  getIsSpecialOf,
+}: {
+  songRecords: Record<string, any>[],
+  sheetRecords: Record<string, any>[],
+  categories: Record<string, any>[],
+  versions: Record<string, any>[],
+  types: Record<string, any>[],
+  difficulties: Record<string, any>[],
+  regions: Record<string, any>[],
+  updateTime?: Date,
+  getLevelValueOf: (sheet: any) => number | null,
+  getIsSpecialOf: (sheet: any) => boolean | null,
+}) {
+  const getSortedSheetsOf = getSheetSorter(
+    { types, difficulties },
+  ).sorted;
+
+  const songs = songRecords.map((song) => ({
+    songId: song.songId,
+
+    category: song.category,
+    title: song.title,
+    artist: song.artist,
+    bpm: song.bpm,
+
+    imageName: song.imageName,
+
+    version: song.version ?? versions.find(
+      ({ dateBefore }) => dateBefore === null || song.releaseDate < dateBefore,
+    )?.version ?? null,
+    releaseDate: song.releaseDate,
+
+    isNew: song.isNew != null ? Boolean(song.isNew) : undefined,
+    isLocked: song.isLocked != null ? Boolean(song.isLocked) : undefined,
+
+    sheets: getSortedSheetsOf(
+      sheetRecords
+        .filter((sheet) => sheet.songId === song.songId)
+        .map((sheet) => ({
+          type: sheet.type,
+          difficulty: sheet.difficulty,
+
+          level: sheet.level,
+          levelValue: getLevelValueOf(sheet),
+
+          noteDesigner: sheet.noteDesigner,
+          noteCounts: sheet.noteCounts ?? undefined,
+
+          regions: sheet.regions != null ? Object.fromEntries(
+            [...Object.entries(sheet.regions)].map(
+              ([region, value]) => [region, Boolean(value)],
+            ),
+          ) : undefined,
+
+          isSpecial: getIsSpecialOf(sheet) || undefined,
+
+          version: sheet.version ?? undefined,
+        })),
+    ),
+  }));
+
+  const output = {
+    songs,
+    categories,
+    versions,
+    types,
+    difficulties,
+    regions,
+    updateTime: updateTime.toISOString(),
+  };
+
+  return JSON.stringify(output, null, '\t');
+}
