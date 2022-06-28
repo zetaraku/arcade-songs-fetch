afbde76b27aed3dd81aee8e6ae091b039334eec7
Raku Zeta <zetaraku@gmail.com>
2022-06-28 19:30:16 +0800

    🩹 core/upload-data: ensure GAME_CODE is set

--- src/core/upload-data.bash
+++ src/core/upload-data.bash
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 # Install AWS CLI version 2 first (https://aws.amazon.com/cli/)
 
+if [ -z "$GAME_CODE" ]; then
+  echo "GAME_CODE is not set, aborted."
+  exit 1
+fi
+
 if [ -z "$S3_BUCKET_NAME" ]; then
   echo "S3_BUCKET_NAME is not set, skipping upload."
-  exit 1
+  exit 2
 fi
 
 if ! command -v aws >/dev/null 2>&1; then
   echo "AWS CLI v2 is not installed. Please install it first: https://aws.amazon.com/cli/"
-  exit 2
+  exit 3
 fi
 
 # Upload data
