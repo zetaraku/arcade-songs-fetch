c5dee0ed25b4c80ad76f0bfe7f0b967b9db7fea4
Raku Zeta <zetaraku@gmail.com>
2022-06-28 22:49:22 +0800

    🚨 eslint: fix no-unused-vars rule for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     'no-restricted-syntax': [
       'error',
       {
