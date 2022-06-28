9e4aedaa9280e8f8a411192d9bbba96412be6f6c
Raku Zeta <zetaraku@gmail.com>
2022-06-28 19:41:02 +0800

    ✨ add core/upload-data script

---
+++ src/core/upload-data.ts
@@ -0,0 +1,25 @@
+/* eslint-disable dot-notation */
+import shell from 'shelljs';
+import log4js from 'log4js';
+import 'dotenv/config';
+
+export default async function run(gameCode: string) {
+  if (!gameCode) {
+    throw new Error('GAME_CODE is not set.');
+  }
+
+  const logger = log4js.getLogger(`${gameCode}/upload-data`);
+  logger.level = log4js.levels.INFO;
+
+  logger.info('Uploading data and images ...');
+  shell.exec('bash src/core/upload-data.bash', {
+    env: {
+      ...process.env,
+      GAME_CODE: gameCode,
+    },
+  });
+
+  logger.info('Done!');
+}
+
+if (require.main === module) run(process.argv[2]);
