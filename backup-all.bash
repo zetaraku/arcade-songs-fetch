584c396b84f3d89030e5729c221f69dda4f7ce2a
Raku Zeta <zetaraku@gmail.com>
2022-07-01 14:13:21 +0800

    🔨 add db backup script

---
+++ backup-all.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+for path in data/*; do
+  if [ -f "$path/db.sqlite3" ]; then
+    echo "Backing up '$(basename $path)' ..."
+    (
+      cd "$path"
+      mkdir -p backup/
+      sqlite3 db.sqlite3 .dump > "backup/$timestamp.sql"
+    )
+  fi
+done
