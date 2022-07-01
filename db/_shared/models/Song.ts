72f77a02a307d9043183e8ed32280cc738b00894
Raku Zeta <zetaraku@gmail.com>
2022-07-01 11:16:47 +0800

    ✨ add Song model factory

---
+++ db/_shared/models/Song.ts
@@ -0,0 +1,23 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+export default function defineModel(sequelize: Sequelize, modelName: string = 'Song') {
+  return sequelize.define(modelName, {
+    songId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+
+    category: DataTypes.STRING,
+    title: DataTypes.STRING,
+    artist: DataTypes.STRING,
+
+    imageName: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+
+    version: DataTypes.STRING,
+    releaseDate: DataTypes.DATEONLY,
+
+    isNew: DataTypes.BOOLEAN,
+    isLocked: DataTypes.BOOLEAN,
+  });
+}
---
+++ db/_shared/models/index.ts
@@ -0,0 +1,1 @@
+export { default as defineSong } from './Song';
