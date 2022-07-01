f18551430e2b4b82d2af82ff17d16b95f07066c3
Raku Zeta <zetaraku@gmail.com>
2022-07-01 11:17:31 +0800

    ♻️ use Song model factory

--- db/jubeat/models/Song.ts
+++ db/jubeat/models/Song.ts
@@ -1,24 +1,6 @@
-import { DataTypes } from 'sequelize';
+import { defineSong } from '@@/db/_shared/models';
 import sequelize from './sequelize';
 
-const Song = sequelize.define('Song', {
-  songId: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-  },
-
-  category: DataTypes.STRING,
-  title: DataTypes.STRING,
-  artist: DataTypes.STRING,
-
-  imageName: DataTypes.STRING,
-  imageUrl: DataTypes.STRING,
-
-  version: DataTypes.STRING,
-  releaseDate: DataTypes.DATEONLY,
-
-  isNew: DataTypes.BOOLEAN,
-  isLocked: DataTypes.BOOLEAN,
-});
+const Song = defineSong(sequelize);
 
 export default Song;
