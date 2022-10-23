c2477217f990477cdf8dac683d726902a919b910
Raku Zeta <zetaraku@gmail.com>
2022-10-24 00:17:20 +0800

    ✨ add postgres example for shared db config factory

--- db/_shared/config/config.js
+++ db/_shared/config/config.js
@@ -13,6 +13,15 @@ const defineConfigs = (gameCode) => ({
     dialect: 'sqlite',
     storage: path.resolve(__dirname, `../../../data/${gameCode}/db.sqlite3`),
   },
+  // development: {
+  //   ...baseConfig,
+  //   dialect: 'postgres',
+  //   host: '127.0.0.1',
+  //   port: '5432',
+  //   username: 'postgres',
+  //   password: '',
+  //   database: gameCode,
+  // },
 });
 
 module.exports = defineConfigs;
