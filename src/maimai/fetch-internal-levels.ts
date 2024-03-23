f970025b46ec2faa9a47e609d83459ec7521454c
Raku Zeta <zetaraku@gmail.com>
2024-03-23 10:08:38 +0800

    🩹 maimai/fetch-internal-levels: update manual mappings

--- src/maimai/fetch-internal-levels.ts
+++ src/maimai/fetch-internal-levels.ts
@@ -21,6 +21,7 @@ const manualMappings = new Map([
   ['Baban!!  ー甘い罠ー', 'BaBan!! －甘い罠－'],
   ['Backyun! -悪い女-', 'Backyun! －悪い女－'],
   ['Bad Apple!! feat.nomico 〜五十嵐撫子Ver.〜', 'Bad Apple!! feat.nomico ～五十嵐 撫子 Ver.～'],
+  ['Bad Apple!! feat.nomico(REDALiCE Remix)', 'Bad Apple!! feat.nomico (REDALiCE Remix)'],
   ['Boys O\'Clock', 'Boys O’Clock'],
   ['Caliburne ～Story of the Legendary Sword～', 'Caliburne ～Story of the Legendary sword～'],
   ['Change Our MIRAI!', 'Change Our MIRAI！'],
