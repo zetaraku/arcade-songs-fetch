25405cefae8f32d51be469aebdb7ea37af39b41d
Raku Zeta <zetaraku@gmail.com>
2025-02-01 21:57:31 +0800

    🗃️ ddr: add JpSheet model

---
+++ db/ddr/migrations/20250201000001-jp-sheet.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+/**
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
+
+async function up(/** @type {QueryInterface} */ queryInterface) {
+  await queryInterface.createTable('JpSheets', {
+    songId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    difficulty: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+  });
+}
+
+async function down(/** @type {QueryInterface} */ queryInterface) {
+  await queryInterface.dropTable('JpSheets');
+}
+
+module.exports = { up, down };
